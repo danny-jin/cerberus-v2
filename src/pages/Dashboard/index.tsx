bbf721d1cec1bf0d2ff611dd66f54ab88cf806a0
danny-jin <icarus96119@gmail.com>
2021-12-28 12:59:52 +0900

    feat: dashboard fixed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { Paper, Grid, Zoom, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import { useSelector } from 'react-redux';
-import React, { useEffect, useState, memo } from 'react';
+import React, { memo } from 'react';
 
 import {
   TotalValueDepositedGraph,
@@ -12,70 +12,78 @@ import {
 } from './Graph';
 import InfoTooltip from '../../components/InfoTooltip';
 import { RootState } from '../../core/store/store';
-import { BaseInfo, BaseInfoKey } from '../../core/interfaces/base';
 import { formatCurrency, formatNumber } from '../../core/utils/base';
-import { defaultNetworkBaseInfos } from '../../core/data/dashboard';
 
 const Dashboard = memo(() => {
 
-  const appData = useSelector((state: RootState) => state.app);
-  const [networkBaseInfos, setNetworkBaseInfos] = useState(defaultNetworkBaseInfos);
-
-  useEffect(() => {
-    if (!appData.currentIndex) {
-      return;
-    }
-    const infos = defaultNetworkBaseInfos;
-    infos.forEach((info: BaseInfo) => {
-      switch (info.key) {
-        case BaseInfoKey.MarketCap:
-          info.value = formatCurrency(appData.marketCap, 0);
-          break;
-        case BaseInfoKey.ThreeDogPrice:
-          info.value = formatCurrency(appData.marketPrice, 4);
-          break;
-        case BaseInfoKey.Apy:
-          info.value = `${new Intl.NumberFormat('en-US').format(Number(formatNumber(appData.stakingApy * 100, 1)))}%`;
-          break;
-        case BaseInfoKey.CirculatingSupply:
-          info.value = new Intl.NumberFormat('en-US').format(parseInt(appData.circulatingSupply));
-          break;
-        case BaseInfoKey.BackingPerThreeDog:
-          info.value = formatCurrency(appData.treasuryMarketValue / appData.circulatingSupply, 4);
-          break;
-        case BaseInfoKey.CurrentIndex:
-          info.value = `${formatNumber(appData.currentIndex, 2)} 3DOGS`;
-          break;
-        default:
-          break;
-      }
-    });
-    setNetworkBaseInfos(infos);
-  }, [appData]);
+  const marketPrice = useSelector((state: RootState) => state.app.marketPrice);
+  const circulatingSupply = useSelector((state: RootState) => state.app.circulatingSupply);
+  const marketCap = useSelector((state: RootState) => state.app.marketCap);
+  const isAppLoading = useSelector((state: RootState) => !state.app?.marketPrice ?? true);
+  const stakingApy = useSelector((state: RootState) => state.app.stakingApy);
+  const backingPerThreeDog = useSelector((state: RootState) => state.app.treasuryMarketValue / state.app.circulatingSupply);
+  const currentIndex = useSelector((state: RootState) => state.app.currentIndex);
 
   return (
     <div className="w-full md:px-30 h-full">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10 rounded-md border-goldsand border-3 w-full p-20 mb-20">
-        {
-          networkBaseInfos.map((info: BaseInfo, index) => {
-            return (
-              <div className="flex flex-col items-start md:items-center" key={index}>
-                <div className="flex items-center">
-                  <Typography variant="h6" color="primary" className="text-center">{info.name}</Typography>
-                  {info?.hasTooltip &&
-                  <div className="ml-5">
-                    <InfoTooltip message={info?.message || ''}/>
-                  </div>
-                  }
-                </div>
-                {!info.value ?
-                  <Skeleton className="w-full h-30"/> :
-                  <Typography variant="h5" color="primary" className="text-center">{info.value}</Typography>
-                }
-              </div>
-            )
-          })
-        }
+        <div className="flex flex-col items-start md:items-center">
+          <div className="flex items-center">
+            <Typography variant="h6" color="primary" className="text-center">Market Cap</Typography>
+          </div>
+          {isAppLoading ?
+            <Skeleton className="w-full h-30"/> :
+            <Typography variant="h5" color="primary" className="text-center">{formatCurrency(marketCap, 0)}</Typography>
+          }
+        </div>
+        <div className="flex flex-col items-start md:items-center">
+          <div className="flex items-center">
+            <Typography variant="h6" color="primary" className="text-center">3DOG Price</Typography>
+          </div>
+          {isAppLoading ?
+            <Skeleton className="w-full h-30"/> :
+            <Typography variant="h5" color="primary" className="text-center">{formatCurrency(marketPrice, 4)}</Typography>
+          }
+        </div>
+        <div className="flex flex-col items-start md:items-center">
+          <div className="flex items-center">
+            <Typography variant="h6" color="primary" className="text-center">APY</Typography>
+          </div>
+          {isAppLoading ?
+            <Skeleton className="w-full h-30"/> :
+            <Typography variant="h5" color="primary" className="text-center">{`${new Intl.NumberFormat('en-US').format(Number(formatNumber(stakingApy * 100, 1)))}%`}</Typography>
+          }
+        </div>
+        <div className="flex flex-col items-start md:items-center">
+          <div className="flex items-center">
+            <Typography variant="h6" color="primary" className="text-center">Circulating Supply (total)</Typography>
+          </div>
+          {isAppLoading ?
+            <Skeleton className="w-full h-30"/> :
+            <Typography variant="h5" color="primary" className="text-center">{new Intl.NumberFormat('en-US').format(parseInt(circulatingSupply))}</Typography>
+          }
+        </div>
+        <div className="flex flex-col items-start md:items-center">
+          <div className="flex items-center">
+            <Typography variant="h6" color="primary" className="text-center">Backing per 3DOG</Typography>
+          </div>
+          {isAppLoading ?
+            <Skeleton className="w-full h-30"/> :
+            <Typography variant="h5" color="primary" className="text-center">{!isNaN(backingPerThreeDog) && formatCurrency(backingPerThreeDog, 4)}</Typography>
+          }
+        </div>
+        <div className="flex flex-col items-start md:items-center">
+          <div className="flex items-center">
+            <Typography variant="h6" color="primary" className="text-center">Current Index</Typography>
+            <div className="ml-5">
+              <InfoTooltip message="The current index tracks the amount of s3DOG accumulated since the beginning of staking. Basically, how much s3DOG one would have if they staked and held a single 3DOG from day 1."/>
+            </div>
+          </div>
+          {isAppLoading ?
+            <Skeleton className="w-full h-30"/> :
+            <Typography variant="h5" color="primary" className="text-center">{`${formatNumber(currentIndex, 2)} 3DOGS`}</Typography>
+          }
+        </div>
       </div>
       <div className="pb-20">
         <Zoom in={true}>
